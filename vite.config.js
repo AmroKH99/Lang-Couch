f3ff88ec32148757dedc09c8f310f4f8d999c48a
redbeard <redbeard@redbeard>
2025-11-15 07:29:25 +0300

    another fix

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-  base: process.env.VITE.BASE.PATH || "/Lang-Couch",
+  base: process.env.VITE_BASE_PATH || "/Lang-Couch",
 })
