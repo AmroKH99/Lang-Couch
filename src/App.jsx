326f8342d95c4aa02ae531795f97b90b4fc53838
redbeard <redbeard@redbeard>
2025-11-16 20:29:04 +0300

    added a font-family

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import DarkVeil from "./components/DarkVeil";
 import LangSelect from "./components/LangSelect";
 import LevelSelect from "./components/LevelSelect";
